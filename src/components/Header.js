9b8fcab1d8bd1746dfce1408a29a335e82964920
guilhermedkdk <guilhermeperesmm@gmail.com>
2023-02-27 10:03:33 -0300

    adding to gitpages

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header">
-            <img src="../images/poker-face.png" className="header--image"/>
+            <img src={process.env.PUBLIC_URL + "/images/poker-face.png"} className="header--image"/>
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">Curso React - Projeto 3</h4>
         </header>
